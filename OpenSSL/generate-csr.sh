297384a1b6fff57c6672b2bb41229bf197803cc8
lilpilgit <62257411+lilpilgit@users.noreply.github.com>
2022-04-01 18:30:05 +0200

    Update generate-csr.sh

--- OpenSSL/generate-csr.sh
+++ OpenSSL/generate-csr.sh
@@ -1,15 +1,24 @@
-openssl req -new -newkey rsa:2048 -nodes -sha256 -keyout example.key -subj "/C=US/ST=CA/O=Organization/OU=Department/CN=example" -config <(
+openssl req -newkey rsa:2048 -nodes -sha256 -keyout example.key -x509 -days 365 -out example.crt -subj /C=US/ST=CA/O=Organization/OU=Department/CN=example -config <(
 cat <<-EOF
 [req]
 default_bits = 2048
 default_md = sha256
-req_extensions = req_ext
 distinguished_name = dn
+x509_extensions = v3_req
 [ dn ]
-[ req_ext ]
+C = US
+ST = VA
+L = SomeCity
+O = MyCompany
+OU = MyDivision
+CN = www.company.com
+[v3_req]
+keyUsage = keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
 subjectAltName = @alt_names
 [alt_names]
 DNS.1 = example.com
 DNS.2 = www.example.com
+DNS.3 = company.net
 EOF
 )
